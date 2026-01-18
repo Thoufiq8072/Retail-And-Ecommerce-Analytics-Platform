ba7c8e65842428a9a9c2d3ec4cb6f74dc13f3caf
Thoufiq8072 <mohammedthoufiq9360@gmail.com>
2026-01-18 11:09:49 +0000

    Gold layer Completion

--- src/schema_definitions.py
+++ src/schema_definitions.py
@@ -73,3 +73,131 @@ SILVER_TRANSACTIONS_SCHEMA = {
     "payment_method":"string",
     "invoice_total":"double"
 }
+
+
+DIM_DATES_SCHEMA = {
+    "date_sk":"integer",
+    "date":"date",
+    "day":"integer",
+    "month":"integer",
+    "month_name":"string",
+    "quarter":"integer",
+    "year":"integer",
+    "day_of_week":"integer",
+    "day_name":"string",
+    "is_weekend":"boolean"
+}
+
+
+DIM_CUSTOMERS_SCHEMA = {
+    "customer_sk":"long",
+    "customer_id":"integer",
+    "name":"string",
+    "email":"string",
+    "telephone":"string",
+    "city":"string",
+    "country":"string",
+    "gender":"string",
+    "date_of_birth":"date",
+    "job_title":"string",
+    "_created_at":"timestamp",
+    "_updated_at":"timestamp"
+}
+
+
+DIM_PRODUCTS_SCHEMA = {
+    "product_sk":"long",
+    "product_id":"integer",
+    "category":"string",
+    "sub_category":"string",
+    "color":"string",
+    "sizes":"string",
+    "production_cost":"double",
+    "description_PT":"string",
+    "description_DE":"string",
+    "description_FR":"string",
+    "description_ES":"string",
+    "description_EN":"string",
+    "description_ZH":"string",
+    "_created_at":"timestamp",
+    "_updated_at":"timestamp"
+}
+
+
+DIM_STORES_SCHEMA = {
+    "store_sk":"long",
+    "store_id":"integer",
+    "store_name":"string",
+    "city":"string",
+    "country":"string",
+    "zip_code":"string",
+    "latitude":"double",
+    "longitude":"double",
+    "number_of_employees":"integer",
+    "_created_at":"timestamp",
+    "_updated_at":"timestamp"
+}
+
+
+DIM_EMPLOYEES_SCHEMA = {
+    "employee_sk":"long",
+    "employee_id":"integer",
+    "store_id":"integer",
+    "name":"string",
+    "position":"string",
+    "_created_at":"timestamp",
+    "_updated_at":"timestamp"
+}
+
+
+DIM_DISCOUNTS_SCHEMA = {
+    "discount_sk":"long",
+    "discount_start_date":"date",
+    "discount_end_date":"date",
+    "discount":"double",
+    "description":"string",
+    "category":"string",
+    "sub_category":"string",
+    "_created_at":"timestamp",
+    "_updated_at":"timestamp"
+}
+
+
+FACT_SALES_SCHEMA = {
+    "sales_sk":"long",
+    "date_sk":"integer",
+    "customer_sk":"long",
+    "product_sk":"long",
+    "store_sk":"long",
+    "employee_sk":"long",
+    "discount_sk":"long",
+    "invoice_id":"string",
+    "line":"integer",
+    "size":"string",
+    "unit_price":"double",
+    "quantity":"integer",
+    "line_total":"double",
+    "invoice_total":"double",
+    "currency":"string",
+    "payment_method":"string",
+    "sale_date":"timestamp",
+    "_created_at": "timestamp"
+}
+
+
+FACT_RETURNS_SCHEMA = {
+    "return_sk":"long",
+    "date_sk":"integer",
+    "customer_sk":"long",
+    "product_sk":"long",
+    "store_sk":"long",
+    "employee_sk":"long",
+    "invoice_id":"string",
+    "line":"integer",
+    "size":"string",
+    "quantity_returned":"integer",
+    "refund_amount":"double",
+    "currency":"string",
+    "sale_date":"timestamp",
+    "_created_at": "timestamp"
+}
--- src/utils.py
+++ src/utils.py
@@ -2,5 +2,12 @@ from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
 def add_ingestion_timestamp(df):
+    """ It will create ingestion_ts and _source_file columns for bronze table metadata"""
     df = df.withColumn("ingestion_ts", current_timestamp()).withColumn("_source_file", col("_metadata.file_path"))
+    return df
+
+
+def add_gold_metadata(df):
+    """ It will create _created_at and _updated_at columns for gold table metadata"""
+    df = df.withColumn("_created_at", current_timestamp()).withColumn("_updated_at", current_timestamp())
     return df
